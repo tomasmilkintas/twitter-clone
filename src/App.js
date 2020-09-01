4dd897b774b6d1087212aefc1507fa47b21f896b
Tomas <tomas.milkintas1@gmail.com>
2020-09-01 11:56:28 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Hello World</h1>
+
+            {/* Sidebar */}
+
+            {/* Feed */}
+
+            {/* Widgets */}
+        </div>
+    );
+}
+
+export default App;
