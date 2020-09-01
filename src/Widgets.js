fc3591c09c20b1097cfccd95ddc0c2bbbe1fcb98
Tomas <tomas.milkintas1@gmail.com>
2020-09-01 16:48:37 +0100

    added widgets

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,9 +1,28 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from "react-twitter-embed";
 
 function Widgets() {
     return (
         <div className="widgets">
-            <h2>This is widgets</h2>
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+
+            <div className="widgets__widgetContainer">
+                <h2>What's happening?</h2>
+                <TwitterTweetEmbed tweetId={"858551177860055040"} />
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="realDonaldTrump"
+                    options={{ height: 400 }}
+                />
+                <TwitterShareButton
+                    url={"https://facebook.com/cleverprogrammer"}
+                    options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+                />
+            </div>
         </div>
     );
 }
