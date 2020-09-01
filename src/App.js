3dd074a2c1eb2c61e7a6063730535c7cb59bffc0
Tomas <tomas.milkintas1@gmail.com>
2020-09-01 13:07:44 +0100

    created sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
     return (
         <div className="app">
             <h1>Hello World</h1>
 
-            {/* Sidebar */}
+            <Sidebar />
 
             {/* Feed */}
 
